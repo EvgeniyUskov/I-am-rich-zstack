eaa7442644009cba5dc9e94fbc07b7269528d441
Evgeniy Uskov <driftrby@gmail.com>
2019-10-09 11:15:33 +0700

    Initial Commit

---
+++ I am rich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I am rich
+//
+//  Created by Evgeniy Uskov on 09/10/2019.
+//  Copyright © 2019 Evgeniy Uskov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
